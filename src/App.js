af3bb5b135f46586d686964f00f099fcc15390ca
sachinm78 <sachinm78@gmail.com>
2019-12-16 13:54:55 -0600

    moved lesson2 to new file and set up lesson3 in App.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,38 @@
 import React, {useState} from "react"
 
-// Convert the class below to a functional component that uses the useState hook to initalize a count vartiable to 0 and display the count on the screen.
-// Don't worry about the part where the button changes the count quite yet, that's what you're here to learn about!
-
 function App() {
-    const [count, setCount] = useState(0)
+    const [inputData, setInputData] = useState({firstName: "", lastName: ""})
+    const [contactsData, setContactsData] = useState([])
     
-    function increment() {
-        setCount(prevCount => prevCount + 1)
+    function handleChange(event) {
+        
     }
     
-    function decrement() {
-        setCount(prevCount => prevCount - 1)
+    function handleSubmit(event) {
+        
     }
     
     return (
-        <div>
-            <h1>{count}</h1>
-            <button onClick={increment}>Increment</button>
-            <button onClick={decrement}>Decrement</button>
-        </div>
+        <>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    placeholder="First Name"
+                    name="firstName" 
+                    value={inputData.firstName}
+                    onChange={handleChange}
+                />
+                <input 
+                    placeholder="Last Name"
+                    name="lastName" 
+                    value={inputData.lastName}
+                    onChange={handleChange}
+                />
+                <br />
+                <button>Add contact</button>
+            </form>
+            {/*{contacts}*/}
+        </>
     )
 }
-
-// class App extends React.Component {
-//     constructor() {
-//         super()
-//         this.state = {
-//             count: 0
-//         }
-//     }
-    
-//     render() {
-//         return (
-//             <div>
-//                 <h1>{this.state.count}</h1>
-//                 <button>Change!</button>
-//             </div>
-//         )
-//     }
-// }
 
 export default App
