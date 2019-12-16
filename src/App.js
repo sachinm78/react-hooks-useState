4bb06ab37f0635bfc45e726d20a4964a17b5380f
sachinm78 <sachinm78@gmail.com>
2019-12-16 13:44:10 -0600

    used useState hook to successfully render a counter and increment button

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import React, {useState} from "react"
 // Don't worry about the part where the button changes the count quite yet, that's what you're here to learn about!
 
 function App() {
-    const [count] = useState(0)
+    const [count, setCount] = useState(0)
     
     return (
-            <div>
-                <h1>{count}</h1>
-                <button>Change!</button>
-            </div>
-        )
+        <div>
+            <h1>{count}</h1>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>Change!</button>
+        </div>
+    )
 }
 
 // class App extends React.Component {
