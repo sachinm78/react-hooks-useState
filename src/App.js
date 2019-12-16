4d7d72d0de5b130bd38f4e1c7c7aaf4e5a9deda4
sachinm78 <sachinm78@gmail.com>
2019-12-16 13:48:50 -0600

    added decrement function and button

--- src/App.js
+++ src/App.js
@@ -10,10 +10,15 @@ function App() {
         setCount(prevCount => prevCount + 1)
     }
     
+    function decrement() {
+        setCount(prevCount => prevCount - 1)
+    }
+    
     return (
         <div>
             <h1>{count}</h1>
             <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
         </div>
     )
 }
