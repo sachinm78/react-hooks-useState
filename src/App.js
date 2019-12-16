f5dbf9cc174b5745a00032e6a8ddfc634ea23869
sachinm78 <sachinm78@gmail.com>
2019-12-16 13:39:48 -0600

    moved lesson1 to new file and set up lesson2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React, {useState} from "react"
 
+// Convert the class below to a functional component that uses the useState hook to initalize a count vartiable to 0 and display the count on the screen.
+// Don't worry about the part where the button changes the count quite yet, that's what you're here to learn about!
+
 function App() {
-    const [answer] = useState("Yes")
+    const [count] = useState(0)
+    
     return (
-        <div>
-            <h1>Is state important to know? {answer}</h1>
-        </div>
-    )
+            <div>
+                <h1>{count}</h1>
+                <button>Change!</button>
+            </div>
+        )
 }
 
 // class App extends React.Component {
 //     constructor() {
 //         super()
 //         this.state = {
-//             answer: "Yes"
+//             count: 0
 //         }
 //     }
     
 //     render() {
 //         return (
 //             <div>
-//                 <h1>Is state important to know? {this.state.answer}</h1>
+//                 <h1>{this.state.count}</h1>
+//                 <button>Change!</button>
 //             </div>
 //         )
 //     }
